cadfc46c589b061799924114a27faf2e58517007
anekkunt <anekkunt@redhat.com>
2015-08-12 11:46:29 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-protoc-c --c_out=. test.proto
+protoc-c --c_out=. test.proto  #generate .c and .h files from test.proto file
 gcc encode.c test.pb-c.c -lprotobuf-c  -o encode
 gcc decode.c test.pb-c.c -lprotobuf-c  -o decode
-./encode |./decode   #pass o/p of encode date to decode using pipe(|) 
+./encode |./decode   #pass o/p of encode to decode using pipe(|) 
